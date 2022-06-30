5f5e559b563c064dc28adc6eaa477c2eb9e97cc2
kauevestena <kauemv2@gmail.com>
2022-06-30 09:08:10 -0300

    data update, small adjustments

--- tests.py
+++ tests.py
@@ -1,7 +1,39 @@
-import altair as alt
-from vega_datasets import data
-import pandas as pd
+def double_scatter_bar(input_df,title,xs='surface',ys='smoothness',scolor=None,xh='count()',yh1='surface',yh2='smoothness',hcolor=None,fontsize=24):
 
-source = data.movies.url
+    interval = alt.selection_interval()
 
-print(pd.DataFrame(source))
+    default_color = alt.value('lightseagreen')
+
+    if not hcolor:
+        hcolor = default_color
+
+    if not scolor:
+        scolor = default_color
+
+
+    scatter = alt.Chart(input_df,title=title).mark_point().encode(
+        x=xs,
+        y=ys,
+        color=scolor,
+        tooltip=alt.Tooltip(['type','id']),
+    ).add_selection(interval)
+
+    hist_base = alt.Chart(input_df).mark_bar().encode(
+        x=xh,
+        color=hcolor,
+        tooltip=alt.Tooltip(['type','id']),
+        
+
+    ).properties(
+        width=300,
+        height=220,
+    ).transform_filter(
+        interval,
+    )
+
+    # if hcolor:
+    #      hist_base.encode(color=hcolor)
+
+    hist = hist_base.encode(y=yh1) | hist_base.encode(y=yh2)
+
+    return (scatter & hist).configure_title(fontSize=fontsize,align='center')
