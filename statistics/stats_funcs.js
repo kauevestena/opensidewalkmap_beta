cf941ea4c3f3af5ac108355b9c64a0777e7f3141
kauevestena <kauemv2@gmail.com>
2023-06-18 19:49:43 -0300

    stats specs first version

--- statistics/stats_funcs.js
+++
@@ -1,9 +0,0 @@
-function responsiveTopNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  } 
-  
