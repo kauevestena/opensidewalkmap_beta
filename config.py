6c653ff7607a3b55273a11df36cd11c71e5d4165
Kauê de Moraes Vestena <kauemv2@gmail.com>
2023-06-21 10:18:04 -0300

    Update config.py

--- config.py
+++ config.py
@@ -19,20 +19,23 @@ REPO_NAME = 'opensidewalkmap_beta'
 # City OSM relation id: (search at  https://nominatim.openstreetmap.org/ui/search.html ):
 CITY_RELATION_ID = 'R297514' #as string!!
 
-# # entire city:
+# BOUNDING BOXES
+# A good tool to find them is: bboxfinder.com
+# # entire city: 
 BOUNDING_BOX = (
-    -25.6450101000000004, # SOUTHMOST
-    -49.3891399999999976, # WESTMOST
-    -25.3467008999999983, # NORTHMOST
-    -49.1843181999999999  # EASTMOST
+    -25.6450101000000004, # SOUTHMOST  LATITUDE
+    -49.3891399999999976, # WESTMOST   LONGITUDE
+    -25.3467008999999983, # NORTHMOST  LATITUDE
+    -49.1843181999999999  # EASTMOST   LONGITUDE
     )
 
-# # a sample of a region of special interest, like the city centre, 
+# # A sample of a region of special interest, like the city centre, 
+# # It must have sidewalks as geometries and be inside the bigger one!!
 BOUNDING_BOX_SAMPLE = (
-    -25.46340831586,     # SOUTHMOST
-    -49.26485433156466,  # WESTMOST
-    -25.45836407828201,  # NORTHMOST
-    -49.257818266840495  # EASTMOST
+    -25.46340831586,     # SOUTHMOST  LATITUDE
+    -49.26485433156466,  # WESTMOST   LONGITUDE
+    -25.45836407828201,  # NORTHMOST  LATITUDE
+    -49.257818266840495  # EASTMOST   LONGITUDE
     )
 
 STREAMLIT_URL='https://kauevestena-opensidewalkmap-beta-streamlit-routing-app-52bins.streamlitapp.com/'
